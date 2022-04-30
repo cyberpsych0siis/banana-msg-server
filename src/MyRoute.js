24418c727bd46beb0284bb94dd7b88eed38ebcb7
Philipp Gaßner <activator112233@gmail.com>
2022-04-30 17:37:43 +0200

    oops typo

--- src/MyRoute.js
+++ src/MyRoute.js
@@ -10,7 +10,7 @@ export default (app) =>  {
     const sql = mysql.createConnection({
         host: process.env.MYSQL_HOST ?? "localhost",
         user: process.env.MYSQL_USER ?? "root",
-        password: process.env.MYSQL_PASSWORD ?? "",
+        password: process.env.MYSQL_PASS ?? "",
         database: process.env.MYSQL_DATABASE ?? "banana-msg"
     });
 
