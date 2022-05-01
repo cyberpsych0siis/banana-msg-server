96ba321e5047b88dbc37aba37f913fa5dbd13387
Philipp Gaßner <activator112233@gmail.com>
2022-05-01 21:10:49 +0200

    my head hurts

--- WebFinger.js/src/sql.js
+++ WebFinger.js/src/sql.js
@@ -14,22 +14,6 @@ export default class SqlSingleton {
         this.sql = sql;
     }
 
-    static loadSql() {
-        try {
-            SqlSingleton.sql = mysql.createConnection({
-                host: process.env.MYSQL_HOST ?? "localhost",
-                user: process.env.MYSQL_USER ?? "root",
-                password: process.env.MYSQL_PASS ?? "",
-                database: process.env.MYSQL_DATABASE ?? "banana-msg"
-            });
-        } catch (e) {
-            console.log(e);
-
-            console.error("Running in Debug Mode")
-            SqlSingleton.sql = new DebugTester()
-        }
-    }
-
     static query(q, data = []) {
         return new Promise((res, rej) => {
             SqlSingleton.sql.query(q, data, (err, dbData) => {
@@ -43,9 +27,9 @@ export default class SqlSingleton {
                     return;
                 }
 
-                console.log(data);
+                console.log(dbData);
     
-                res(data);
+                res(dbData);
             });
         });
     }
